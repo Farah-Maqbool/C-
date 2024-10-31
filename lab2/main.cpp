67384c110545b26284340b0fc4c7d414e94955ae
Farah Maqbool <farahprogrammer23@gmail.com>
2024-10-31 21:57:58 +0500

    lab

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,23 +3,23 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-////	Que 1
-//	int x[] = {2,4,5,1,6,7,9};
-//	int size = sizeof(x) / sizeof(int);
-//	int max = x[0];
-//	for (int i = 0 ; i < size ; i++){
-//		if (x[i]>max){
-//			max = x[i];
-//		}
-//	}
-//	cout << "Maximum Value: " << max << endl;
+//	Que 1
+	int x[] = {2,4,5,1,6,7,9};
+	int size = sizeof(x) / sizeof(int);
+	int max = x[0];
+	for (int i = 0 ; i < size ; i++){
+		if (x[i]>max){
+			max = x[i];
+		}
+	}
+	cout << "Maximum Value: " << max << endl;
 //	Que 2
 	int w[5];
 	w[0] = 1;
 	w[1] = 3;
 	w[2] = 5;
 	w[3] = 4;
-	cout << "Befor" << endl;
+	cout << "Before" << endl;
 	for  (int i = 0 ; i < 5 ; i++){
 		cout << w[i] << endl;
 	}
@@ -31,5 +31,6 @@ int main() {
 	for  (int i = 0 ; i < 5 ; i++){
 		cout << w[i] << endl;
 	}
+//	Que 3
 	return 0;
 }
