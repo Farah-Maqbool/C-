5c3faf078ac6732f674abb0e835ee70b1f4d09ef
Farah Maqbool <farahprogrammer23@gmail.com>
2024-11-01 21:20:41 +0500

    lab

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
 //	Que 1
+	cout << "Question 1" << endl;
 	int x[] = {2,4,5,1,6,7,9};
 	int size = sizeof(x) / sizeof(int);
 	int max = x[0];
@@ -14,6 +15,7 @@ int main() {
 	}
 	cout << "Maximum Value: " << max << endl;
 //	Que 2
+	cout << "Question 2" << endl;
 	int w[5];
 	w[0] = 1;
 	w[1] = 3;
@@ -32,5 +34,46 @@ int main() {
 		cout << w[i] << endl;
 	}
 //	Que 3
+	cout << "Question 3" << endl;
+	int l[5];
+	l[0] = 4;
+	l[1] = 5;
+	l[2] = 2;
+	l[4] = 0;
+	for (int i = 0 ; i < 5 ; i++){
+		if (l[i]==0){
+			l[i-1] = 6;
+			break;
+		}
+	}
+	for (int i = 0 ; i < 5 ; i++){
+		cout << l[i] << endl;
+	}
+//	Que 4
+	cout << "Question 4" << endl;
+	int E[7];
+	E[0] = 3;
+	E[1] = 5;
+	E[2] = 2;
+	E[3] = 4;
+	E[4] = 7;
+	E[5] = 3;
+	
+	int give = 4;
+	cout << "Before" << endl;
+	for (int i = 0 ; i < 7 ; i++){
+		cout << E[i] << endl ;
+	}
+	for (int i = 5 ; i >= 0 ; i++){
+		E[i+1] = E[i];
+		if (i == give){
+			break;
+		}
+	}
+	E[give] = 10;
+	cout << "After";
+	for (int i = 0 ; i < 7 ; i++){
+		cout << E[i] << endl ;
+	}
 	return 0;
 }
