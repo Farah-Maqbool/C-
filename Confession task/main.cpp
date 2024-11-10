ed9c419c5843797b197b5c3e87a6dfba69591254
Farah Maqbool <farahprogrammer23@gmail.com>
2024-11-10 19:05:32 +0500

    task

--- Confession task/main.cpp
+++ Confession task/main.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
 using namespace std;
 
-void roastFeedback(int time){
+void dailyroastFeedback(int time){
 	if (time < 60){
 		cout << "Chalo, kam az kam tumne apni zindagi ke 1 ghante se kam hi barbaad kiye. Isi tarah control mein raho aur asli zindagi mein bhi kuch kar dikhane ka irada rakho" << endl;
 	}
 	else if (time >= 60 && time <= 120){
 		cout << "Vah, lagta hai aj sirf confession scroll karne ka irada tha. 1 se 2 ghante waste karke samajhte ho tumhari life set ho jayegi? Thora focus apne real goals pe bhi rakho, shayad kuch ban jao." << endl;
 	}
+	else if(time > 120){
+		cout << "Oh bhai, 2 ghante confessions mein laga diye?! Kya NASA mein job lagne wali hai is se? Acha hota yehi time kisi kaam ki cheez pe lagate. Zindagi mein kuch bara karna hai ya bas yahan hi phasna hai?" << endl;
+	}
+}
+void weeklyRoastFeedbck(int time){
+	cout << "Weekly Roast" << endl;
+	if (time > 600){
+		cout << "Kya baat hai, 10 ghante se zyada confessions mein ghusa hua hai? Jawan admi ho ya waqt barbaad karne ki machine? Kuch productive cheez socho, warna ye confessions tumhari CV mein nahi likhe jayenge!" << endl;
+	}
+	else if(time < 600){
+		cout << "Wah bhai, lagta hai thoda waqt manage karna seekh rahe ho! Isi tarah apne goals par focus karo, warna ye confession wali duniya tumhe kabhi aagay nahi le jaayegi. Shabash, sahi raho!" << endl;
+	}
+	
 }
 
-
-int calculateTotalTime(int read, int write, int* check){
-	int* ptr = check;
+void calculateTotalTime(int read, int write, int &check){
 	int week[7];
-	int size = sizeof(week) / sizeof(int);
 	int total = read + write;
-	for (int i = 0 ; i < size ; i++){
-		week[*ptr]=total;
-	}
-	*ptr++;
-	if (*ptr==6){
+	
+	if (check==6){
 		int total_week_data = 0;
-		for (int i = 0 ; i < *ptr ; i++){
+		for (int i = 0 ; i < check ; i++){
 			total_week_data += week[i];
 		}
-		roastFeedback(total_week_data);
+		weeklyRoastFeedbck(total_week_data);
+		check = 0;
 	}
-	else{
-		roastFeedback(total);
-	}
-	return 0;
+	dailyroastFeedback(total);
+	week[check]=total;
+	check++;
+	
 }
 
 
@@ -46,7 +54,7 @@ int main(int argc, char** argv) {
 		cin >> read;
 		cout << "Time Spent on writing Confession (in minutes): ";
 		cin >> write;
-		calculateTotalTime(read, write, &check);
+		calculateTotalTime(read, write, check);
 		cout << "Want to continue? (y or n) ";
 		cin >> user_choice;
 		if (user_choice=='n'){
