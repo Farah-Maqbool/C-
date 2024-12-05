75828992ddc3ee512a06e9ce1de52a64883898bb
Farah Maqbool <farahprogrammer23@gmail.com>
2024-12-05 11:20:34 +0500

    program

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -6,23 +6,16 @@ int main() {
 //	Que 1
 	int arr_1[3][2] = {{1,2},{3,4},{5,6}};
 	int arr_2[3][2] = {{7,8},{9,10},{11,12}};
-	int arr[6][2];
+	int arr[3][2];
 	
 	for (int i = 0 ; i < 3 ; i++){
 		for (int j = 0 ; j < 2 ; j++){
-			arr[i][j] = arr_1[i][j];
+			arr[i][j] = arr_1[i][j] + arr_2[i][j];
 			
 		}
 	}
-	int num = 3;
+	
 	for (int i = 0 ; i < 3 ; i++){
-		for (int j = 0 ; j < 2 ; j++){
-			arr[num][j] = arr_2[i][j];
-			
-		}
-		num++;
-	}
-	for (int i = 0 ; i < 6 ; i++){
 		for (int j = 0 ; j < 2 ; j++){
 			cout << arr[i][j] << endl;
 		}
